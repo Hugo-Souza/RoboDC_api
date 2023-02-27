abb0b0c010d48970254689dddf4725ea35fbcbda
Hugo Souza <hugo.souza@estudante.ufscar.br>
2023-02-27 20:33:45 -0300

    api test

--- api2/api.py
+++ api2/api.py
@@ -1,25 +1,13 @@
 from flask import Flask, request, jsonify
-import subprocess
+from subprocess import call
 
 app = Flask(__name__)
 
 @app.route('/run_program', methods=['POST'])
 def run_program():
-    program = request.files.get('program')
-    if program:
-        program.save(program.filename)
-        try:
-            output = subprocess.check_output(['python', program.filename], stderr=subprocess.STDOUT, timeout=30)
-            result = {'status': 'success', 'output': output.decode('utf-8')}
-        except subprocess.CalledProcessError as e:
-            result = {'status': 'error', 'message': e.output.decode('utf-8')}
-        except Exception as e:
-            result = {'status': 'error', 'message': str(e)}
-        finally:
-            os.remove(program.filename)
-    else:
-        result = {'status': 'error', 'message': 'No program provided'}
-    return jsonify(result)
+    call('python ./missions/send_goal_LE1.py', shell=True)
+    
+
 
 if __name__ == '__main__':
     app.run()
