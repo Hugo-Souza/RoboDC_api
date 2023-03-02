b60431db9cf4a218f8f80c4c801c67d8d1797be9
souzaitor <heitor.souza@vonbraunlabs.com.br>
2023-03-02 08:25:54 -0300

    feat: Endpoint pra retornar lista de locais

--- src/controllers/ros_controller.py
+++ src/controllers/ros_controller.py
@@ -7,16 +7,53 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 api = Namespace("ros", description="ROS controller")
 
+available_locals = {
+    "LE-1": (-37.99, -5.45, 1.0, 0.0),
+    "LE-2": (-30.15, -5.03, 1.0, 0.0),
+    "LE-3": (-22.68, -4.45, 1.0, 0.0),
+    "LE-4": (-15.36, -4.11, 1.0, 0.0),
+    "Suporte": (-11.30, -3.92, 1.0, 0.0),
+    "PPG-CC4": (-2.54, -3.12, 1.0, 0.0),
+    "Maker": (7.46, -2.39, 1.0, 0.0),
+    "LE-5": (9.75, -2.36, 1.0, 0.0),
+    "Auditorio": (15.37, -1.86, 1.0, 0.0),
+    "Banheiros": (-38.74, -10.59, 1.0, 0.0),
+    "Copa": (-38.43, -16.47, 1.0, 0.0),
+    "Lig": (-38.01, -22.61, 1.0, 0.0),
+    "Reunioes": (-15.52, -23.80, 1.0, 0.0),
+    "Chefia": (-12.49, -23.54, 1.0, 0.0),
+    "Graduacao": (-18.67, -24.17, 1.0, 0.0),
+    "Recepcao": (-12.49, -23.54, 1.0, 0.0),
+    "Home": (-1.65, -21.18, 1.0, 0.0)
+}
+
+
+class goal(Resource):
+    def get(self):
+        """Retorna a lista de todos os destinos possíveis do robô."""
+
+        locals_list = list(available_locals.keys())
+            
+        return { "available_locals": locals_list }, 200
+
+
 class ROS(Resource):
     def get(self, location):
-        result = movebase_client(location)
-        
+        """Faz o robô se movimentar até o local recebido.
+        Se o local recebido não estiver cadastrado, o robô se movimenta até o saguão do DC."""
+
+        #result = movebase_client(location)
+        result = teste(location)
+
         if result:
             return { "result": f"Deu certo! { str(result) }" }, 200
         else:
             return { "error": "Erro" }, 400
 
+
+api.add_resource(goal, "/goal")
 api.add_resource(ROS, "/goTo/<location>")
+
 
 def movebase_client(local):
 
@@ -28,97 +65,17 @@ def movebase_client(local):
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
 
-    if local == "LE-1":
-        goal.target_pose.pose.position.x = -37.99 
-        goal.target_pose.pose.position.y = -5.45
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "LE-2":
-        goal.target_pose.pose.position.x = -30.15
-        goal.target_pose.pose.position.y = -5.03
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "LE-3":
-        goal.target_pose.pose.position.x = -22.68
-        goal.target_pose.pose.position.y = -4.45
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "LE-4":
-        goal.target_pose.pose.position.x = -15.36
-        goal.target_pose.pose.position.y = -4.11
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "Suporte":
-        goal.target_pose.pose.position.x = -11.30
-        goal.target_pose.pose.position.y = -3.92
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "PPG-CC4":
-        goal.target_pose.pose.position.x = -2.54
-        goal.target_pose.pose.position.y = -3.12
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "Maker":
-        goal.target_pose.pose.position.x = 7.46
-        goal.target_pose.pose.position.y = -2.39
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "LE-5":
-        goal.target_pose.pose.position.x = 9.75
-        goal.target_pose.pose.position.y = -2.36
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "Auditorio":
-        goal.target_pose.pose.position.x = 15.37
-        goal.target_pose.pose.position.y = -1.86
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "Banheiros":
-        goal.target_pose.pose.position.x = -38.74
-        goal.target_pose.pose.position.y = -10.59
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "Copa":
-        goal.target_pose.pose.position.x = -38.43
-        goal.target_pose.pose.position.y = -16.47
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "Lig":
-        goal.target_pose.pose.position.x = -38.01
-        goal.target_pose.pose.position.y = -22.61
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "Reunioes":
-        goal.target_pose.pose.position.x = -15.52
-        goal.target_pose.pose.position.y = -23.80
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "Chefia":
-        goal.target_pose.pose.position.x = -12.49
-        goal.target_pose.pose.position.y = -23.54
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "Graduacao":
-        goal.target_pose.pose.position.x = -18.67
-        goal.target_pose.pose.position.y = -24.17
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "Recepcao":
-        goal.target_pose.pose.position.x = -12.49
-        goal.target_pose.pose.position.y = -23.54
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    elif local == "Home":
-        goal.target_pose.pose.position.x = -1.65
-        goal.target_pose.pose.position.y = -21.18
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-    else: #home
-        goal.target_pose.pose.position.x = -1.65
-        goal.target_pose.pose.position.y = -21.18
-        goal.target_pose.pose.orientation.z = 1.0 
-        goal.target_pose.pose.orientation.w = 0.0
-
+    if local in available_locals:
+        goal.target_pose.pose.position.x = available_locals[local][0]
+        goal.target_pose.pose.position.y = available_locals[local][1]
+        goal.target_pose.pose.orientation.z = available_locals[local][2]
+        goal.target_pose.pose.orientation.w = available_locals[local][3]
+    else:
+        goal.target_pose.pose.position.x = available_locals["Home"][0]
+        goal.target_pose.pose.position.y = available_locals["Home"][1]
+        goal.target_pose.pose.orientation.z = available_locals["Home"][2]
+        goal.target_pose.pose.orientation.w = available_locals["Home"][3]
+  
     client.send_goal(goal)
 
     wait = client.wait_for_result()
